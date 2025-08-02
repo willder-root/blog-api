1ca42bf2e5a674445dee4d70faf6ea962c3e51d5
willder-root <willderaraujo_99_gds@hotmail.com>
2025-08-02 08:20:49 -0300

    :sparkles: Criação de servido http com express

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
+
+
+
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,5 @@ import mongoose from 'mongoose';
 
 const connectString = `${process.env.MONGO_URI}`
 mongoose.connect(connectString).then(()=>{
-    console.log("Conexão ao mongoDB inicializada");
-    
+    console.log("Conexão ao mongoDB inicializada");    
 });
